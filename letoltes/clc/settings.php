f703cc2750203198fe39473591dc7786b0c08c74
lionkmp <lion@netngine.hu>
2019-12-31 08:50:13 +0000

    Deleted CLC merger program. Kept only a note about it's original function. Not in use anymore.

--- letoltes/clc/settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define("PROXY_ENABLED",false);
-    define("HTTP_PROXY", "proxy.address:3128");
-    define("CLC_DATA_FOLDER","./");
-    define("CLC_DATA_CHARSET","UTF-8");
-    define("DEBUG_ENABLED", false);
-    
-?>
