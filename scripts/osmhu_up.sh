7f41275050630cc272bb98c8a5e75ae21c283980
lionkmp <lion@netngine.hu>
2021-01-08 14:16:26 +0000

    Fixes to osmhu_up scripts to match current project status.
    No vendor dir. Links in node_modules were not copied.
    Added hints about --delete for node_modules.

--- scripts/osmhu_up.sh
+++ scripts/osmhu_up.sh
@@ -2,9 +2,9 @@
 cd ~/w/osmhu
 
 # Fully synced dirs to upload
-for i in build config css includes kepek node_modules query vendor
-do 
-	rsync --progress -v -r -t --exclude '.svn' "$i" c64:w/osmhu/
+for i in build config css includes kepek node_modules query
+do
+	rsync --progress -v -r -t -l --exclude '.svn' "$i" c64:w/osmhu/
 done
 
 # files to up
@@ -13,4 +13,5 @@ rsync favicon.ico lib.php terkep.php validate*.php c64:w/osmhu/
 # HTML
 rsync *.shtml *.xml c64:w/osmhu/
 
-
+# To clean up run this with no dry: be careful.
+#rsync -rtv --progress -l --delete --dry-run node_modules/ c64:w/osmhu/node_modules/
