0a6649676f2b768acbf5ba163b2f166230205f8d
fodor0205 <fodor0205@gmail.com>
2023-09-26 00:41:08 +0200

    Add docker support

--- config/development.php
+++ config/development.php
@@ -1,13 +1,13 @@
 <?php
 
 // MySQL connection details
+define('DB_HOST', isset($_ENV['DB_HOST']) ? $_ENV['DB_HOST'] : '127.0.0.1');
 define('DB_USERNAME', 'osmhu');
 define('DB_PASSWORD', 'Eidoh5zo');
-define('DB_HOST', '127.0.0.1');
 define('DB_DATABASE', 'osm_hu');
 
 // PostgreSQL connection details
+define('PGSQL_HOST', isset($_ENV['PGSQL_HOST']) ? $_ENV['PGSQL_HOST'] : '127.0.0.1');
 define('PGSQL_USERNAME', 'osmhu');
 define('PGSQL_PASSWORD', 'Eidoh5zo');
-define('PGSQL_HOST', '127.0.0.1');
 define('PGSQL_DATABASE', 'gis');
